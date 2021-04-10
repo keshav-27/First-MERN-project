eab6ef4309bad251988b9e6601ec7bc6e6f8ae62
keshav-27 <64672728+keshav-27@users.noreply.github.com>
2021-04-10 14:35:11 +0530

    folder structure not proper

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import './styles.css'
+import {HomePage} from './components/homepage/homepage.component'
+
+function App() {
+    return(
+        <div>
+        <HomePage />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const HomePage = () => {
+    return(
+        <div className= "homepage">
+            <div className= "directory-menu">
+                <div className= "menu-item">
+                    <div className= "content">
+                    <h1 className= "title">HATS</h1>
+                    <span className= "subtitle">Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
